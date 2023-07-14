c18e8994f8e9300b4b3f8d84083a72891607b447
Pavel Sanicovich <rapidcodelab.mx@gmail.com>
2023-07-15 01:28:55 +0300

    showmode false

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -1,7 +1,6 @@
 local conditions = require("heirline.conditions")
 local utils = require("heirline.utils")
 
-
 local Space = { provider = " " }
 
 local colors = require("kanagawa.colors").setup() -- wink
--- lua/rapidcodelab/settings.lua
+++ lua/rapidcodelab/settings.lua
@@ -3,5 +3,6 @@ local o = vim.o
 
 o.number = true 
 o.relativenumber = true
+o.showmode = false
 
 g.mapleader = " "
