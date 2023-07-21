f85fc5c41abb352c702ab14f8debc23996596e62
RapidCodeLab <rapidcodelab.mx@gmail.com>
2023-07-21 17:42:33 +0300

    cmp added

--- lua/rapidcodelab/plugins.lua
+++ lua/rapidcodelab/plugins.lua
@@ -37,6 +37,13 @@ local plugins = {
   {"williamboman/mason.nvim"},
   {"williamboman/mason-lspconfig.nvim"},
 	{"neovim/nvim-lspconfig"},
+  {"hrsh7th/cmp-nvim-lsp"},
+  {"hrsh7th/cmp-buffer"},
+  {"hrsh7th/cmp-path"},
+  {"hrsh7th/cmp-cmdline"},
+  {"hrsh7th/nvim-cmp"},
+  {"SirVer/ultisnips"},
+  {"quangnguyen30192/cmp-nvim-ultisnips"},
 }
 local opts = {}
 
