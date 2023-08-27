33d8f0c5a93c8aa8932466584fd0710e8c3ef6e1
RapidCodeLab <rapidcodelab.mx@gmail.com>
2023-08-27 05:57:52 +0300

    new keymaps

--- lua/rapidcodelab/keymaps.lua
+++ lua/rapidcodelab/keymaps.lua
@@ -13,6 +13,18 @@ map('n', '<leader>c', ':bd<CR>', default_opts) --close buffer
 
 
 map('n', '<leader>tt', ':TransparentToggle<CR>', default_opts)
-map('n', '<leader>mt', ':Mason<CR>', default_opts)
+map('n', '<leader>tm', ':Mason<CR>', default_opts)
 
 map('n', '<leader>gf', ':GoFmt<CR>', default_opts)
+
+
+
+--splits && movs
+map('n', '<leader>\\', ':split<CR>', default_opts)
+map('n', '<leader>|', ':vsplit<CR>', default_opts)
+map('n', '<leader>aw', ':wall<CR>', default_opts)
+
+map('n', '<leader>mh', '<C-w>h', default_opts)
+map('n', '<leader>mj', '<C-w>j', default_opts)
+map('n', '<leader>mk', '<C-w>k', default_opts)
+map('n', '<leader>ml', '<C-w>l', default_opts)
